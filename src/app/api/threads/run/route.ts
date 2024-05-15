9b186873fc19b46499e32cd53b0ad4d858ec8b98
Henry Li <henry1943@163.com>
2024-05-15 22:22:48 +0800

    feat: add GET

--- src/app/api/threads/run/route.ts
+++ src/app/api/threads/run/route.ts
@@ -2,9 +2,32 @@ import { type NextRequest } from 'next/server';
 
 import { EventStreamResponse } from '~/server/streaming';
 import { ServerThread } from '~/server/threads';
+import { type Thread } from '~/shared/threads';
 
 export async function POST(request: NextRequest) {
-  const json = await request.json();
-  const thread = new ServerThread(json);
+  const thread = (await request.json()) as Thread;
+  return runThread(thread);
+}
+
+export function GET(request: NextRequest) {
+  const question =
+    request.nextUrl.searchParams.get('q') ?? '介绍一下《幕府将军》这部剧';
+  const thread: Thread = {
+    id: '1eed32dfa',
+    messages: [
+      {
+        id: 'f8b0ce48a',
+        type: 'text',
+        role: 'user',
+        content: question,
+      },
+    ],
+    createdTime: Date.now(),
+  };
+  return runThread(thread);
+}
+
+async function runThread(threadData: Thread) {
+  const thread = new ServerThread(threadData);
   return new EventStreamResponse(thread.run());
 }
