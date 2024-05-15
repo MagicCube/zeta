292f762895020c50012c8ece0aaf40a233adc967
Henry Li <lixin.henry@bytedance.com>
2024-05-15 18:56:53 +0800

    feat: add `new thread` button

--- src/app/_components/_main/index.tsx
+++ src/app/_components/_main/index.tsx
@@ -1,8 +1,15 @@
 'use client';
 
+import IconEdit from '@mui/icons-material/Edit';
+import Button from '@mui/joy/Button';
 import { useCallback } from 'react';
 
-import { getActiveThread, useActiveThread } from '~/client/store';
+import {
+  activateThread,
+  createThread,
+  getActiveThread,
+  useActiveThread,
+} from '~/client/store';
 
 import { useVisualViewport } from '../../hooks';
 import AppHeader from '../app-header';
@@ -18,13 +25,23 @@ export default function Main() {
     const thread = getActiveThread();
     await thread.sendMessage(value);
   }, []);
+  const handleNewThread = useCallback(() => {
+    const newThread = createThread();
+    activateThread(newThread);
+  }, []);
   return (
     <div className={styles.container} {...containerProps}>
       <header className={styles.header}>
-        <AppHeader />
+        <AppHeader
+          right={
+            <Button variant="plain" size="sm" onClick={handleNewThread}>
+              <IconEdit />
+            </Button>
+          }
+        />
       </header>
       <main ref={scrollableRef} className={styles.main}>
-        <MessageListView thread={thread} />
+        <MessageListView className={styles.messageListView} thread={thread} />
       </main>
       <footer className={styles.footer} {...targetProps}>
         <UserInput
