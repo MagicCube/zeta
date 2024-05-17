b94a96d3560e50e9183d8017ab9348197c416e2d
Henry Li <henry1943@163.com>
2024-05-18 01:08:49 +0800

    feat: rename

--- src/server/tools/search-tool/renderer.ts
+++ src/server/tools/search-tool/renderer.ts
@@ -1,7 +1,7 @@
 import { type SearchResponse, type SearchEntry, type SearchSubject } from '..';
 
 export function renderSearchResponse(res: SearchResponse) {
-  return `# Search Results
+  return `# Retrieval Results
 ${renderSubject(res.subject)}
 ${renderSearchResult(res.organicResults)}`;
 }
