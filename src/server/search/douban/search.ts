5b6e7a4fad9add51097c07655ac1c4793a170f8e
Henry Li <henry1943@163.com>
2024-05-17 20:58:07 +0800

    chore: remove console

--- src/server/search/douban/search.ts
+++ src/server/search/douban/search.ts
@@ -1,7 +1,6 @@
 import { type DoubanSearchResult } from '.';
 
 export async function searchMovies({ q, count }: { q: string; count: number }) {
-  console.info(process.env.DOUBAN_API_KEY);
   const res = await fetch('https://api.douban.com/v2/movie/search', {
     method: 'post',
     headers: {
